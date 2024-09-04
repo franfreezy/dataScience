6ab30508715a9a927c59d6f416f3a4b97758ad13
franfreezy <frandelwanjawa@yahoo.com>
2024-09-05 02:19:51 +0300

    Process the data to support condensing to a better format that will incorporate bcr

--- UN_datasets_and_analysis/populationGrowthUrbanNCapitalsAfrica.py
+++ UN_datasets_and_analysis/populationGrowthUrbanNCapitalsAfrica.py
@@ -1,4 +1,11 @@
+#import modules
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+import pandas as pd
+
+#DATA CLEANUP EXERCISE
 
 #path to files
 countrycsv='africancounties.csv'
@@ -20,8 +27,16 @@ with open(countrycsv, newline='') as csvfile,open(datasetcsv, newline='') as csv
         countryname=data[1]
         requireddata=[data[1],data[2],data[4],data[6]]
         capitalpopln='Capital city population (thousands)'
-        if countryname in country:
-
-            if capitalpopln in data[3]:
-                print(requireddata)
         
+        
+        if countryname in country:
+            if capitalpopln in data[3]:
+
+                print(requireddata) # conversion of the data into a dataframe
+    
+
+#DATAVISUALISATION
+np.random.seed(42) #ensure visualized data is the same all the time
+city_bars = 54 # number of bars
+city_frames = 20 #rate at which they are scanned, the more, the slower.
+

0e7fa92b23db0b0c8c73d2e9b8bff8ec2d781bcb
franfreezy <frandelwanjawa@yahoo.com>
2024-09-07 01:29:52 +0300

    blanks filled up

--- UN_datasets_and_analysis/populationGrowthUrbanNCapitalsAfrica.py
+++ UN_datasets_and_analysis/populationGrowthUrbanNCapitalsAfrica.py
@@ -20,32 +20,48 @@ with open(countrycsv, newline='') as csvfile,open(datasetcsv, newline='') as csv
     next(reader2)
     writer.writerow(['Country', 'year', 'city', 'population', ])
     country=[]
-    dict18 = {}
-    dict05 = {}
-    dict15 = {}
-    dict10 = {}
+    
 
     for row in reader1:
         country.append(row[0])
 
     for data in reader2:
         countryname = data[1]
-        
-
-        
-        
         capitalpopln='Capital city population (thousands)'
-        
-        
+      
         if countryname in country:
             
             if data[4] !='':
                 
                 if capitalpopln in data[3]:
                     writer.writerow([data[1], data[2], data[4], data[6]])
-                
-                    
-                    
+#version 1 csv generated
+v1csv = 'capitalcityv1.csv'
+v2csv='output.csv'
+with open(v2csv, mode='w', newline='') as csvfile, open(v1csv, newline='') as csvfile2:
+    reader=csv.reader(csvfile2)
+    writer = csv.writer(csvfile)
+    required_years = ['2005', '2010', '2015', '2018']              
+     # Write the header
+    data_by_country = {}
+
+    for row in reader:
+        country, year, city, population = row
+        if country not in data_by_country:
+            data_by_country[country] = {}
+        data_by_country[country][year] = (city, population)
+        for country in data_by_country:
+            for year in required_years:
+                if year not in data_by_country[country]:
+                    # Fill missing data with placeholders
+                    data_by_country[country][year] = (city, 0)
+
+    for country, years_data in data_by_country.items():
+        for year in required_years:
+            city, population = years_data[year]
+            writer.writerow([country, year, city, population])
+    
+#refine the output with the blanks filled up                    
                 
     
        

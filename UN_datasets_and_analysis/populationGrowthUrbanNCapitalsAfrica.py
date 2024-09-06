e2ef021bd4f00b2ae52d1bb910940a04b68d6d96
franfreezy <frandelwanjawa@yahoo.com>
2024-09-06 21:24:12 +0300

    All data worked on and in 3 dictionaries

--- UN_datasets_and_analysis/populationGrowthUrbanNCapitalsAfrica.py
+++ UN_datasets_and_analysis/populationGrowthUrbanNCapitalsAfrica.py
@@ -19,7 +19,9 @@ with open(countrycsv, newline='') as csvfile,open(datasetcsv, newline='') as csv
     next(reader2)
     
     country=[]
-    
+    dict18 = {}
+    dict05 = {}
+    dict15={}
     for row in reader1:
         country.append(row[0])
 
@@ -31,12 +33,20 @@ with open(countrycsv, newline='') as csvfile,open(datasetcsv, newline='') as csv
         
         if countryname in country:
             if capitalpopln in data[3]:
+                if (requireddata[1]) == '2015':  # conversion of the data into a dataframe
+                    dict15[requireddata[2]] = requireddata[3]
+                elif (requireddata[1]) == '2005':
+                    dict05[requireddata[2]] = requireddata[3]
+                elif (requireddata[1]) == '2018':
+                    dict18[requireddata[2]] = requireddata[3]
+print(dict18)   
+print(dict05)
+print(dict15)
 
-                print(requireddata) # conversion of the data into a dataframe
-    
 
 #DATAVISUALISATION
 np.random.seed(42) #ensure visualized data is the same all the time
 city_bars = 54 # number of bars
 city_frames = 20 #rate at which they are scanned, the more, the slower.
+
 

205822fad2c75bb725367fd9e3cd8c5e2f8fb4a4
franfreezy <frandelwanjawa@yahoo.com>
2024-09-11 00:48:09 +0300

    3 seconds of amazing work

--- UN_datasets_and_analysis/popgrowthcapitals/poplgrowthcapitals.py
+++ UN_datasets_and_analysis/popgrowthcapitals/poplgrowthcapitals.py
@@ -13,7 +13,23 @@ actual_popln.iloc[:, 1:]*=1000
 data='output3.csv'
 df = pd.read_csv(data)
 actual_popln2 = df.copy()
-actual_popln2*=1000
-print(actual_popln2)
+actual_popln2 *= 1000
+df = actual_popln2
+
+new_column_data = ['2005', '2010', '2015', '2018']
+df.insert(0, 'Year', new_column_data)
+df = df.set_index('Year')
+
+bcr.bar_chart_race(
+    df=df,
+    filename='bar_chart_race.mp4',  
+    title='African Capital cities population growth', 
+    figsize=(15, 10),  
+    cmap='dark12',  
+    n_bars=53,  
+    period_length=1200,  
+    label_bars=True, 
+    sort='desc'  
+)
 
 

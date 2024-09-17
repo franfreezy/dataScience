5ab069604318a24bd3079386a0bf5f7af674a313
franfreezy <frandelwanjawa@yahoo.com>
2024-09-17 16:17:39 +0300

    Filled up the blank cities.

--- UN_datasets_and_analysis/popgrowthcapitalsWorld/poplngrowthcapitalcountries.py
+++ UN_datasets_and_analysis/popgrowthcapitalsWorld/poplngrowthcapitalcountries.py
@@ -25,3 +25,37 @@ with open(datasetcsv, newline='') as csvfile,open(csv_file, mode='w', newline=''
         capitalpopln = 'Capital city population (thousands)'
         if capitalpopln in data[3]:
             writer.writerow([data[1], data[2], data[4], data[6]])
+
+csv_file = "capitalcityv1.csv"
+csv_file2 = 'output.csv'
+with open(csv_file2, mode='w', newline='') as csvfile, open(csv_file, newline='') as csvfile2:
+    reader=csv.reader(csvfile2)
+    writer = csv.writer(csvfile)
+    required_years = ['2005', '2010', '2015', '2018']              
+     # Write the header
+    data_by_country = {}
+
+    for row in reader:
+        country, year, city, population = row
+        if country not in data_by_country:
+            data_by_country[country] = {}
+        data_by_country[country][year] = (city, population)
+        for country in data_by_country:
+            for year in required_years:
+                if year not in data_by_country[country]:
+                    # Fill missing data with placeholders
+                    data_by_country[country][year] = (city, 0)
+
+    for country, years_data in data_by_country.items():
+        for year in required_years:
+            city, population = years_data[year]
+            writer.writerow([country, year, city, population])
+
+v1csv = 'output.csv'
+v2csv='output1.csv'
+with open(v2csv, mode='w', newline='') as csvfile, open(v1csv, newline='') as csvfile2:
+    reader = list(csv.reader(csvfile2))
+    writer = csv.writer(csvfile)               
+    writer.writerow(['Country', 'year', 'city', 'population',])
+    writer.writerows(reader[4:])
+
